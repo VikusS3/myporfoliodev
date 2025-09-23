712839dbbbcd0f1a82514cdd079b38d6c51a9047
SaulDV <felix21soc@gmail.com>
2025-09-23 09:14:13 -0500

    Add SEO tags and config, update favicon and homepage
    Introduces SEOtags component and config data/types for improved SEO management. Adds og-image.jpg, updates favicon, and enhances homepage and layout to utilize new SEO features. Also adds astro-seo dependency.

---
+++ src/types/config.ts
@@ -0,0 +1,3 @@
+export const DEFAUL_TITLE_PAGE = "SaulDev";
+export const DEFAUL_DESCRIPTION_PAGE =
+  "Desarrollador de software con 2 años de experiencia, me encanta crear sitios web y aplicaciones web, soy un apasionado de la tecnología y la programación.";
